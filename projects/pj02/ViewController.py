5dfbf9b1080465c9acf1de3af012844a2b525234
Kyle Orciuch <korciuch@stanford.edu>
2021-11-11 20:45:21 -0800

    grid shading done

--- projects/pj02/ViewController.py
+++ projects/pj02/ViewController.py
@@ -1,7 +1,8 @@
 """The ViewController drives the visualization of the simulation.""" 
 
-from turtle import Turtle, Screen, done
-from projects.pj02.model import Model
+import numpy as np
+from turtle import Turtle, Screen, color, done
+from projects.pj02.model import Model, Point
 from projects.pj02 import constants
 from typing import Any
 from time import time_ns, sleep
@@ -49,19 +50,43 @@ class ViewController:
             self.pen.pendown()
             self.pen.goto(constants.MAX_X,y_coord)
 
+    def fill_grid(self) -> None:
+        def fill_square(x_start,y_start,c):
+            delta_x = constants.BOUNDS_HEIGHT / constants.NUM_COLS
+            delta_y = constants.BOUNDS_WIDTH / constants.NUM_ROWS
+            self.pen.penup()
+            self.pen.goto(x_start,y_start)
+            self.pen.pendown()
+            self.pen.fillcolor(c)
+            self.pen.begin_fill()
+            self.pen.goto(x_start,y_start-delta_y)
+            self.pen.goto(x_start+delta_x,y_start-delta_y)
+            self.pen.goto(x_start+delta_x,y_start)
+            self.pen.goto(x_start,y_start)
+            self.pen.end_fill()
+        self.screen.colormode(255)
+        upper_left = Point(constants.MIN_X,constants.MAX_Y)
+        for i in range(0,constants.NUM_COLS):
+            for j in range(0,constants.NUM_ROWS):
+                c = tuple(np.random.random_integers(1,255,size=3))
+                delta_x = constants.BOUNDS_WIDTH / constants.NUM_COLS * i
+                delta_y = constants.BOUNDS_HEIGHT / constants.NUM_ROWS * j
+                fill_square(upper_left.x+delta_x,upper_left.y-delta_y,c)
+
     def tick(self) -> None:
         """Update the model state and redraw visualization."""
         start_time = time_ns() // NS_TO_MS
         self.model.tick()
         self.pen.clear()
         self.initialize_grid()
+        self.fill_grid()
         sleep(1)
         for cell in self.model.population:
             self.pen.penup()
             self.pen.goto(cell.location.x, cell.location.y)
             self.pen.pendown()
             self.pen.color(cell.color())
-            self.pen.dot(constants.CELL_RADIUS)
+            self.pen.dot(constants.CELL_RADIUS-5)
         self.screen.update()
 
         if self.model.is_complete():
