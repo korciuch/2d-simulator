608b600a5d5097dcc67036ca6d88b60567eede0e
Kyle Orciuch <korciuch@stanford.edu>
2021-11-15 12:35:32 -0800

    q-learn

---
+++ q-learning.py
@@ -0,0 +1,56 @@
+import csv
+import sys
+import numpy as np
+import pandas as pd
+
+from collections import defaultdict
+
+STATE = 0
+ACTION = 1
+REWARD = 2
+NEW_STATE = 3
+NUM_EPOCHS = int(sys.argv[4])
+
+def load_samples(infile):
+    data = []
+    with open(infile) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar="\"")
+        count = -1
+        for r in reader:
+            count += 1
+            if count == 0: continue
+            data.append(np.subtract(np.asarray(r, dtype=np.int64),1))
+    return np.asarray(data)
+
+def compute(infile,outfile):
+    D = load_samples(infile)
+    D[:,REWARD] = np.add(D[:,REWARD],1)
+    Q = q_learning(data=D,alpha=0.5,gamma=0.95)
+    export_policy(matrix=Q,output=outfile,offset=1)
+
+def q_learning(data, alpha, gamma):
+    Q_shape = (int(sys.argv[3]),np.max(data[:,ACTION])+1)
+    Q = np.zeros(shape=Q_shape, dtype=np.float64)
+    ii64 = np.iinfo(np.int64)
+    Q.fill(ii64.min)
+    for e in range(0,NUM_EPOCHS):
+        learning_rate = alpha * ((NUM_EPOCHS - e) / NUM_EPOCHS)**2
+        for s in data:
+            Q[s[STATE],s[ACTION]] += learning_rate * (s[REWARD] + gamma * np.max(Q[s[NEW_STATE],:]) - Q[s[STATE],s[ACTION]])
+    print(Q)
+    return Q
+
+def export_policy(matrix,output,offset):
+    with open(output, 'a') as f:
+        for state_row in matrix:
+            f.write(str(np.argmax(state_row)+offset)+'\n')
+
+def main():
+    if len(sys.argv) != 5:
+        raise Exception("usage: python project2.py <infile>.csv <outfile>.policy num_states num_epochs")
+    inputfilename = sys.argv[1]
+    outputfilename = sys.argv[2]
+    compute(inputfilename, outputfilename)
+
+if __name__ == '__main__':
+    main()
--- simulation.py
+++ simulation.py
@@ -1,22 +1,70 @@
 import numpy as np
 
+END_REWARD = 1000
+
 def main():
     m_rows = 10
     n_columns = 10
 
-    gs = (m_rows,n_columns)
-    a = ['up', 'down', 'left', 'right']
-    T = create_transition_matrix(grid_shape=gs, actions=a)
+    
+    a = [(1,0), (-1,0), (0,-1), (0,1)] # down - 1, up - 2, left - 3, right - 4
+    world_gs = (m_rows,n_columns)
+    transition_gs = (len(a), m_rows * n_columns, n_columns * m_rows)
 
-    r = np.random.uniform(-1,1,size=((m_rows,n_columns)))
+    #T = create_transition_matrix(tgrid_shape=transition_gs, actions=a,wgrid_shape=world_gs)
+
+    r = np.random.randint(-10,10,size=((m_rows,n_columns)))
+    print(r)
     ss = (m_rows-1,0)
     es = (0,n_columns-1)
+    r[es] = END_REWARD
+    explore(r,a,n_samples=100000)
 
-def create_transition_matrix(grid_shape, actions):
-    pass
+# [0,0, ..., 0,n]     [0, 1, 2] --> i.e. np.ravel_multi_index((0,0), (3,3)) --> 0
+# [..., ..., ...] --> [3, 4, 5] --> i.e. np.ravel_multi_index((1,1), (3,3)) --> 4
+# [m,0, ..., m,n]     [6, 7, 8] --> i.e. np.ravel_multi_index((2,2), (3,3)) --> 8
 
-def explore(reward_matrix, start_state, end_state):
-    pass
+"""def create_transition_matrix(tgrid_shape, actions, wgrid_shape):
+    def is_passable(orig,dest,action) -> bool:
+        o_coord = np.unravel_index(orig,wgrid_shape) 
+        d_coord = np.unravel_index(dest,wgrid_shape)
+        print(o_coord, action)
+        print(d_coord, action)
+
+    T = np.zeros(shape=(tgrid_shape))
+    print(T.shape)
+    for a in range(0, T.shape[0]):
+        for o in range(0, T.shape[1]):
+            for d in range(0, T.shape[2]):
+                if is_passable(o, d, actions[a]): T[a,o,d] = 1"""
+
+
+def explore(reward_matrix, actions, n_samples):
+
+    def export_samples(samples):
+        with open('simulation.csv', 'w') as f:
+            f.write('s,a,r,s\''+'\n')
+            for s in samples:
+                f.write(','.join(s)+'\n')
+
+    samples = []
+    for _ in range(n_samples):
+        current_state = np.random.randint(0, np.shape(reward_matrix)[0]*np.shape(reward_matrix)[1])
+        state = np.unravel_index(current_state, np.shape(reward_matrix))
+        index = np.random.choice(len(actions))
+        a = actions[index]
+        m = state[0] + a[0] 
+        n = state[1] + a[1]
+        if m < 0 or n < 0: continue
+        elif m >= np.shape(reward_matrix)[0] or n >= np.shape(reward_matrix)[1]: continue
+        sp = (m, n)
+        #print(state, a, sp, index+1)
+        r = reward_matrix[m,n]
+        next_state = np.ravel_multi_index(sp, np.shape(reward_matrix))
+        samples.append([str(current_state+1), str(index+1), str(r), str(next_state+1)])
+
+    #print(samples)
+    export_samples(samples)
 
 if __name__ == "__main__":
     main()
