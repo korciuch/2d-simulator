6d5920c9abb06115663f3a29e72f7f5bb234b4d2
Ryan Ludwick <rludwick@stanford.edu>
2021-12-05 15:02:25 -0800

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -10,17 +10,20 @@ def create_reward_matrix(create_new):
     n_columns = 25
     ss = (m_rows-1,0)
     es = (0,n_columns-1)
+    ii64 = np.iinfo(np.int64)
     if create_new:
-        r = np.ones((m_rows,n_columns), dtype=int)
+        r = np.ones((m_rows,n_columns), dtype=np.int64)
         #r = np.random.randint(-100,100,size=((m_rows,n_columns)))
         #r[0:10, :] = 1
         #r[:, 6] = 1
         #r[:, 12] = 1
         #r[19:24, :] = 1
-        r[2:4, :-2] = -1000
-        r[7:9, 3:25] = -1000
-        r[14:16, :-3] = -1000
-        r[20:22, 3:25] = -1000
+        r[2:4, :-2] = -1001
+        r[7:9, 3:25] = -1001
+        r[12:18, 1:9] = -1001
+        r[12:18, 12:16] = -1001
+        r[12:18, 19:23] = -1001
+        r[21:23, 3:24] = -1001
         r[es] = END_REWARD
         r[ss] = START_REWARD
         # export reward matrix
